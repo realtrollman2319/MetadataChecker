ad66f27f1ae836d39d4c2f84142c855e49c2f6a3
realtrollman <realtrollman2292929@gmail.com>
2023-01-13 02:21:49 +0800

    Added player.giveItemMetadata(uInt16 itemId, string MetadataString)

--- MetadataChecker/MetadataChecker.cs
+++ MetadataChecker/MetadataChecker.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using uScript.API.Attributes;
 using uScript.Core;
 using uScript.Module.Main.Classes;
@@ -14,6 +17,30 @@ namespace MetadataChecker
             if (!(instance.Data is ItemClass item)) return 0;
             var metadata = item.Item.item.metadata.Select(b => (ExpressionValue)b); // Converts each byte in the byte array to an ExpressionValue
             return ExpressionValue.Array(metadata);
+        }
+    }
+
+    [ScriptTypeExtension(typeof(PlayerClass))]
+    public class MetadataItemSpawner
+    {
+        [ScriptFunction]
+        public static void giveItemMetadata([ScriptInstance] ExpressionValue instance, ushort itemId, string metadataString)
+        {
+            if (!(instance.Data is PlayerClass playerClass)) return;
+            if (string.IsNullOrEmpty(metadataString)) return;
+
+            string[] bytes = metadataString.Split(',');
+            List<byte> bList = new List<byte>();
+
+            foreach (string b in bytes)
+            {
+                bList.Add(Byte.Parse(b));
+            }
+
+            Item newItem = new Item(itemId, 1, 100);
+            newItem.metadata = bList.ToArray();
+
+            playerClass.Player.inventory.forceAddItem(newItem, true);
         }
     }
 }
