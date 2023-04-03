c8783232966559d1737346b77fdd0c5c22f21b38
realtrollman2319 <108993392+realtrollman2319@users.noreply.github.com>
2023-04-03 23:53:14 +0800

    passengers describe an 8 hour from hell

--- MetadataChecker/MetadataChecker.cs
+++ MetadataChecker/MetadataChecker.cs
@@ -3,12 +3,82 @@ using Steamworks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 using uScript.API.Attributes;
 using uScript.Core;
 using uScript.Module.Main.Classes;
 
 namespace MetadataChecker
 {
+    [ScriptTypeExtension(typeof(InventoryClass))]
+    public class MetadataItemSpawner2
+    {
+        [ScriptFunction("addItemMetadata")]
+        public static void addItemMetadata([ScriptInstance] ExpressionValue instance, ushort idGiven, ExpressionValue arrayGiven, byte qualityGiven = 100, byte amountGiven = 1) // For uScript module developers, you can set optional arguments by setting a default variable like qualityGiven, and amountGiven.
+        {
+            if (!(instance.Data is InventoryClass iclass)) return;
+
+            Asset findAsset = Assets.find(EAssetType.ITEM, idGiven); // Find item
+
+            if (findAsset != null) // Yes it exists
+            {
+                List<byte> bList = new List<byte>(); // Create list
+
+                foreach (var arrayElement in arrayGiven.AsList) // Loop through
+                {
+                    byte parsedByte;
+                    bool hasParsedArrayElementByte = Byte.TryParse(arrayElement, out parsedByte);
+                    if (hasParsedArrayElementByte == true)
+                    {
+                        bList.Add(parsedByte); // Yay, valid array element! Let's add it.
+                    }
+                    else
+                    {
+                        Console.WriteLine("MetadataChecker module from uScript => One of the array elements failed to parse! Numbers must only be 0 to 255 and not a string or an object. Check the array elements and try again.", Console.ForegroundColor = ConsoleColor.Red); // Asshole put "255" lmao
+                        Console.ResetColor();
+                        return;
+                    }
+                }
+
+                Item newItem = new Item(idGiven, 1, qualityGiven, bList.ToArray()); // Create an item
+                ItemClass newUItemClass = new ItemClass(idGiven);
+                int uItemLength = newUItemClass.Item.item.metadata.Length;
+
+                if (uItemLength == 0) // Item dosen't have metadata (like a potato)
+                {
+                    Console.WriteLine("MetadataChecker module from uScript => Item's metadata dosen't contain an array! Spawn another item.", Console.ForegroundColor = ConsoleColor.Red); // metadata dosen't exist
+                    Console.ResetColor();
+                    return;
+                }
+                else if (uItemLength < bList.Count) // Item metadata is shorter than array given
+                {
+                    Console.WriteLine($"MetadataChecker module from uScript => Given array exceeds the given item's metadata! Edit your array to match the metadata you're editing. Item's metadata count: {uItemLength} | Array count: {bList.Count}", Console.ForegroundColor = ConsoleColor.Red);
+                    Console.ResetColor();
+                    return;
+                }
+                else if (uItemLength > bList.Count) // Item metadata is larger than array given
+                {
+                    Console.WriteLine($"MetadataChecker module from uScript => Given array is smaller than the item's metadata! Edit your array to match the metadata you're editing. Item's metadata count: {uItemLength} | Array count: {bList.Count}", Console.ForegroundColor = ConsoleColor.Red);
+                    Console.ResetColor();
+                    return;
+                }
+                else
+                {
+                    for (int i = 0; i < amountGiven; i++)
+                    {
+                        iclass.Inventory.tryAddItem(newItem, true);
+                    }
+                }
+            }
+            else // No it dosen't again lmao
+            {
+                Console.WriteLine("MetadataChecker module from uScript => Item not found.", Console.ForegroundColor = ConsoleColor.Red);
+                Console.ResetColor();
+                return;
+            }
+        }
+    }
+
     [ScriptTypeExtension(typeof(ItemClass))]
     public class MetadataChecker
     {
@@ -16,62 +86,28 @@ namespace MetadataChecker
         public static ExpressionValue getMetadata([ScriptInstance] ExpressionValue instance)
         {
             if (!(instance.Data is ItemClass item)) return ExpressionValue.Null;
-            var metadata = item.Item.item.metadata.Select(b => (ExpressionValue)b); // Converts each byte in the byte array to an ExpressionValue
+            var metadata = item.Item.item.metadata.Select(b => (ExpressionValue)b); // Converts each byte in the byte array to an ExpressionValue - Ster
             return ExpressionValue.Array(metadata);
-        }
-    }
-
-    [ScriptTypeExtension(typeof(PlayerClass))]
-    public class MetadataItemSpawner
-    {
-        [ScriptFunction]
-        public static void giveItemMetadata([ScriptInstance] ExpressionValue instance, ushort itemId, ExpressionValue arrayGiven)
-        {
-            if (!(instance.Data is PlayerClass playerClass)) return;
-
-            List<byte> bList = new List<byte>();
-
-            foreach (var arrayElement in arrayGiven.AsList)
-            {
-                byte parsedByte;
-                bool hasParsedArrayElementByte = Byte.TryParse(arrayElement, out parsedByte);
-
-                if (hasParsedArrayElementByte == true)
-                {
-                    bList.Add(parsedByte);
-                }
-                else
-                {
-                    Console.WriteLine("MetadataChecker module from uScript => One of the array elements failed to parse! Numbers must only be 0 to 255 and not a string or an object. Check the array elements and try again.", Console.ForegroundColor = ConsoleColor.Red);
-                    Console.ResetColor();
-                    return;
-                }
-            }
-
-            Item newItem = new Item(itemId, 1, 100);
-            newItem.metadata = bList.ToArray();
-
-            playerClass.Player.inventory.forceAddItem(newItem, true);
         }
     }
 
     [ScriptModule("MetadataEditor")]
     public class MetadataEditorClass
     {
-        [ScriptFunction]
+        [ScriptFunction("applyMetadata")]
         public static void applyMetadata(ItemClass itemGiven, string playerIdGiven, ExpressionValue arrayGiven)
         {
-            List<byte> bList = new List<byte>();
+            List<byte> bList = new List<byte>(); // Create a list
 
-            foreach (var arrayElement in arrayGiven.AsList)
+            foreach (var arrayElement in arrayGiven.AsList) // Loop through
             {
                 byte parsedByte;
                 bool hasParsedArrayElementByte = Byte.TryParse(arrayElement, out parsedByte);
-                if (hasParsedArrayElementByte == true)
+                if (hasParsedArrayElementByte == true) // It is a byte
                 {
-                    bList.Add(parsedByte);
+                    bList.Add(parsedByte); // Add it to the array
                 }
-                else
+                else // No it isn't again
                 {
                     Console.WriteLine("MetadataChecker module from uScript => One of the array elements failed to parse! Numbers must only be 0 to 255 and not a string or an object. Check the array elements and try again.", Console.ForegroundColor = ConsoleColor.Red);
                     Console.ResetColor();
@@ -80,7 +116,7 @@ namespace MetadataChecker
             }
 
             ulong parsedPlayerID;
-            bool hasParsedPlayerID = UInt64.TryParse(playerIdGiven, out parsedPlayerID);
+            bool hasParsedPlayerID = UInt64.TryParse(playerIdGiven, out parsedPlayerID); // Parse player ID
             if (hasParsedPlayerID != true)
             {
                 Console.WriteLine("MetadataChecker module from uScript => The player ID failed to parse! Check the player ID argument and see if it's a string with numbers only included.", Console.ForegroundColor = ConsoleColor.Red);
@@ -88,7 +124,7 @@ namespace MetadataChecker
                 return;
             }
 
-            Player playerGiven = PlayerTool.getPlayer(new CSteamID(parsedPlayerID));
+            Player playerGiven = PlayerTool.getPlayer(new CSteamID(parsedPlayerID)); // Get player from ID
             if (playerGiven == null)
             {
                 Console.WriteLine("MetadataChecker module from uScript => Player dosen't exist on the game! Check if you've entered the correct player ID and the player is currently in-game.", Console.ForegroundColor = ConsoleColor.Red);
@@ -100,39 +136,34 @@ namespace MetadataChecker
             bool hasFoundItem = false;
 
             Items[] items = playerGiven.inventory.items;
-            foreach (Items itemsElement in items)
+            foreach (Items itemsElement in items) // Loop through player inventory
             {
-                if (itemsElement.containsItem(itemGiven.Item))
+                if (itemsElement.containsItem(itemGiven.Item)) // Does the item contain the item?
                 {
-                    itemPage = itemsElement.page;
+                    itemPage = itemsElement.page; // Set page
                     hasFoundItem = true;
                     break;
                 }
             }
 
-            if (hasFoundItem != true)
+            if (hasFoundItem == true)
             {
-                Console.WriteLine("MetadataChecker module from uScript => Item not found. This should probably not happen unless the module creator made a error making this module.", Console.ForegroundColor = ConsoleColor.Red);
-                Console.ResetColor();
-                return;
-            }
-            else
-            {
-                if (itemGiven.Item.item.metadata.Length == 0)
+                int iLength = itemGiven.Item.item.metadata.Length;
+                if (iLength == 0) // Item dosen't have metadata (like a potato)
                 {
-                    Console.WriteLine("MetadataChecker module from uScript => Item's metadata dosen't contain an array! Edit another item.", Console.ForegroundColor = ConsoleColor.Red);
+                    Console.WriteLine("MetadataChecker module from uScript => Item's metadata dosen't contain an array! Edit another item.", Console.ForegroundColor = ConsoleColor.Red); // metadata dosen't exist
                     Console.ResetColor();
                     return;
                 }
-                else if (itemGiven.Item.item.metadata.Length < bList.Count)
+                else if (iLength < bList.Count) // Item metadata is shorter than array given
                 {
-                    Console.WriteLine("MetadataChecker module from uScript => Given array exceeds the given item's metadata! Edit your array to match the metadata you're editing. Item's metadata count: " + itemGiven.Item.item.metadata.Length, Console.ForegroundColor = ConsoleColor.Red);
+                    Console.WriteLine($"MetadataChecker module from uScript => Given array exceeds the given item's metadata! Edit your array to match the metadata you're editing. Item's metadata count: {iLength} | Array count: {bList.Count}", Console.ForegroundColor = ConsoleColor.Red);
                     Console.ResetColor();
                     return;
                 }
-                else if (itemGiven.Item.item.metadata.Length > bList.Count)
+                else if (iLength > bList.Count) // Item metadata is larger than array given
                 {
-                    Console.WriteLine("MetadataChecker module from uScript => Given array is smaller than the item's metadata! Edit your array to match the metadata you're editing. Item's metadata count: " + itemGiven.Item.item.metadata.Length, Console.ForegroundColor = ConsoleColor.Red);
+                    Console.WriteLine($"MetadataChecker module from uScript => Given array is smaller than the item's metadata! Edit your array to match the metadata you're editing. Item's metadata count: {iLength} | Array count: {bList.Count}", Console.ForegroundColor = ConsoleColor.Red);
                     Console.ResetColor();
                     return;
                 }
@@ -145,16 +176,97 @@ namespace MetadataChecker
                     playerGiven.inventory.sendUpdateInvState(itemPage, itemGiven.Item.x, itemGiven.Item.y, bList.ToArray());
                 }
             }
+            else
+            {
+                Console.WriteLine("MetadataChecker module from uScript => Item not found. Please report issue on GitHub.", Console.ForegroundColor = ConsoleColor.Red);
+                Console.ResetColor();
+                return;
+            }
         }
-        
-        [ScriptFunction]
-        public static ExpressionValue toByteArray(int intGiven)
-        {
-            byte[] split = new byte[2];
-            Buffer.BlockCopy(BitConverter.GetBytes(intGiven), 0, split, 0, 2);
 
-            var byteArray = split.Select(b => (ExpressionValue)b); // Converts each byte in the byte array to an ExpressionValue - Ster
+        [ScriptFunction("toByteArray")]
+        public static ExpressionValue toByteArray(ushort ushortGiven) // Turn uInt16 to 2 bytes
+        {
+            byte[] bArray = BitConverter.GetBytes(ushortGiven);
+            var byteArray = bArray.Select(b => (ExpressionValue)b); // Converts each byte in the byte array to an ExpressionValue - Ster
             return ExpressionValue.Array(byteArray);
+        }
+
+        [ScriptFunction("toUInt16")]
+        public static ExpressionValue toUInt16(byte byte1given, byte byte2given) // Turn 2 bytes to uInt16
+        {
+            byte[] bytes = new byte[2] { byte1given, byte2given };
+            return BitConverter.ToUInt16(bytes, 0);
+        }
+    }
+
+    [ScriptTypeExtension(typeof(PlayerClass))]
+    public class MetadataItemSpawner
+    {
+        [ScriptFunction("giveItemMetadata")]
+        public static void giveItemMetadata([ScriptInstance] ExpressionValue instance, ushort idGiven, ExpressionValue arrayGiven, byte qualityGiven = 100, byte amountGiven = 1)
+        {
+            if (!(instance.Data is PlayerClass playerClass)) return;
+
+            Asset findAsset = Assets.find(EAssetType.ITEM, idGiven); // Find the asset
+
+            if (findAsset != null) // If it's not null (It exists)
+            {
+                List<byte> bList = new List<byte>(); // Create a byte list
+
+                foreach (var arrayElement in arrayGiven.AsList)
+                {
+                    byte parsedByte;
+                    bool hasParsedArrayElementByte = Byte.TryParse(arrayElement, out parsedByte); // Try to parse
+
+                    if (hasParsedArrayElementByte == true) // Yes it exists
+                    {
+                        bList.Add(parsedByte);
+                    }
+                    else // No it dosen't lmao, now we tell player
+                    {
+                        Console.WriteLine("MetadataChecker module from uScript => One of the array elements failed to parse! Numbers must only be 0 to 255 and not a string or an object. Check the array elements and try again.", Console.ForegroundColor = ConsoleColor.Red);
+                        Console.ResetColor();
+                        return;
+                    }
+                }
+
+                Item newItem = new Item(idGiven, 1, qualityGiven, bList.ToArray()); // Create an item
+                ItemClass newUItemClass = new ItemClass(idGiven);
+                int uItemLength = newUItemClass.Item.item.metadata.Length;
+
+                if (uItemLength == 0) // Item dosen't have metadata (like a potato)
+                {
+                    Console.WriteLine("MetadataChecker module from uScript => Item's metadata dosen't contain an array! Spawn another item.", Console.ForegroundColor = ConsoleColor.Red); // Metadata dosen't exist
+                    Console.ResetColor();
+                    return;
+                }
+                else if (uItemLength < bList.Count) // Item metadata is shorter than array given
+                {
+                    Console.WriteLine($"MetadataChecker module from uScript => Given array exceeds the given item's metadata! Edit your array to match the metadata you're editing. Item's metadata count: {uItemLength} | Array count: {bList.Count}", Console.ForegroundColor = ConsoleColor.Red);
+                    Console.ResetColor();
+                    return;
+                }
+                else if (uItemLength > bList.Count) // Item metadata is larger than array given
+                {
+                    Console.WriteLine($"MetadataChecker module from uScript => Given array is smaller than the item's metadata! Edit your array to match the metadata you're editing. Item's metadata count: {uItemLength} | Array count: {bList.Count}", Console.ForegroundColor = ConsoleColor.Red);
+                    Console.ResetColor();
+                    return;
+                }
+                else
+                {
+                    for (int i = 0; i < amountGiven; i++)
+                    {
+                        playerClass.Player.inventory.forceAddItem(newItem, true); // Add the item to player's inventory | TBH I should've removed this method and replace it to inventory.addItemMetadata, but that would fuck up scripts
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("MetadataChecker module from uScript => Item not found.", Console.ForegroundColor = ConsoleColor.Red);
+                Console.ResetColor();
+                return;
+            }
         }
     }
 }
